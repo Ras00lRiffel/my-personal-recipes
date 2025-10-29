d3eeb2b25d139226d1a19033643c20466475c5a7
Rasool Riffel <riffelrasool@gmail.com>
2025-10-29 23:32:43 +0300

    #myrecipes: adding the header and some boilerplate styles for typography

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,20 @@
-import "../../assets/styles/Header/_header.scss";
+import logo from "../../assets/images/logo-v2.png";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="flex flex-row">
-        <div className="basis-2/4 pr-4 pl-4">
-          <img src="" alt="" />
+      <div className="flex flex-row items-center">
+        <div className="basis-1/4 pr-4 pl-4">
+          <img className="header-logo" src={logo} alt="My Recipes Logo" />
         </div>
-        <div className="basis-2/4 pr-4 pl-4">world</div>
+        <div className="basis-3/4 pr-4 pl-4">
+          <h1>Explore My Recipes</h1>
+        </div>
+      </div>
+      <div className="heading-desc">
+        <p>
+          Browse through my favorite recipes - simple, heary and made with love.
+        </p>
       </div>
     </div>
   );
